33bf89c2ab4fdf5f161962cb451f442f1e32e2f5
Fernnado <Ferdinhokirei666@gmail.com>
2026-09-15 23:54:47 -0300

    feat: recupera nova palavra com SavedStateHandle

--- src/main/java/br/edu/ifsp/scl/sc3018237/intentscompose/MainActivity.kt
+++ src/main/java/br/edu/ifsp/scl/sc3018237/intentscompose/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -60,7 +61,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HomeScreen(navController: NavController){
-    var stringAtual = ""
+    var stringAtual by remember {
+        mutableStateOf("")
+    }
+
+    val novaPalavra by navController
+        .currentBackStackEntry!!
+        .savedStateHandle
+        .getStateFlow("novaPalavra","")
+        .collectAsState()
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(20.dp),
